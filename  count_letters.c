a2a13444077f2287dbfb62f5e03362cdcc4145ec
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-31 14:04:21 +0100

    son

---  count_letters.c
+++  count_letters.c
@@ -1,32 +1,43 @@
+/*
+
+                  Count vowels and consonants
+                  Given a string, print count of vowels and consonants in the string.
+
+Input Format
+Input contains a string S, consisting of lowercase and uppercase characters.
+
+Constraints
+1 <= len(S) <= 100
+
+Output Format
+Print count of vowels and consonants in the given string, separated by space.
+
+Sample Input 0
+aBxbbiAasPw
+
+Sample Output 0
+4 7
+
+Explanation 0
+Self Explanatory
+
+*/
+
 #include <stdio.h>
-
-int main() {
-    char str[1000];
-    int voyelles = 0, consonnes = 0;
-
-    fgets(str, sizeof(str), stdin); // lecture avec espaces
-
-    for (int i = 0; str[i] != '\0'; i++) {
-        char c = str[i];
-
-        // Mettre les majuscules en minuscules manuellement
-        if (c >= 'A' && c <= 'Z') {
-            c = c + 32;
-        }
-
-        // Vérifie si c'est une lettre
-        if ((c >= 'a') && (c <= 'z')) {
-            // Voyelles
-            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y') {
-                voyelles++;
-            } else {
-                consonnes++;
-            }
-        }
+int main()
+{
+    char str[100];
+    int i,vow,con;
+    i=vow=con=0;
+    
+    gets(str);
+    while (str[i]!='\0')
+    {
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' || str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U')
+              vow++;      
+          else
+            con++;
+        i++;
     }
-
-    printf("Voyelles : %d\n", voyelles);
-    printf("Consonnes : %d\n", consonnes);
-
-    return 0;
+    printf("%d %d", vow,con);
 }
