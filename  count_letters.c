4e4e530db1d5e0ce1fe04c086ddf407eac5259ad
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-31 11:57:03 +0100

    port

---  count_letters.c
+++  count_letters.c
@@ -3,20 +3,22 @@
 
 int main() {
     char name[50];
-    int i, vc=0, cc=0;
+    int i, vc = 0, cc = 0;
 
-    scanf("%s", name);
-    for ( i = 0; i < strlen(name); i++)
-    return 0;
-    {
+    // Lecture d’une chaîne avec espaces :
+    scanf("%49s", name);  // "%s" ne lit pas les espaces. Si tu veux une phrase entière : "%49[^\n]"
+
+    for (i = 0; i < strlen(name); i++) {
         if (name[i] == 'a' || name[i] == 'e' || name[i] == 'i' || name[i] == 'o' || name[i] == 'u' ||
-            name[i] == 'A' || name[i] == 'E' || name[i] == 'I' || name[i] == 'O' || name[i] == 'U') 
-            vc=vc+1;
-         else
-            cc=cc+1;
+            name[i] == 'A' || name[i] == 'E' || name[i] == 'I' || name[i] == 'O' || name[i] == 'U') {
+            vc++;
+        } else {
+            cc++;
         }
-    printf("\n Voyelles : %d", vc);
-    printf("\n Consonnes : %d", cc);
-    return 0;
     }
-    
+
+    printf("Voyelles : %d\n", vc);
+    printf("Consonnes : %d\n", cc);
+
+    return 0;
+}
