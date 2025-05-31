3ebbb22813b48981b153d64195bbdc07b2343750
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-31 14:41:24 +0100

    bien

---  count_letters.c
+++  count_letters.c
@@ -1,68 +1,23 @@
-// C program to count special characters, digits,
-// vowels, and consonants in a given string
+//Write a program to count the number of vowels and consonants in a string.
+
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
 #include <string.h>
-
-// Function to count special characters, digits,
-// vowels, and consonants in a given string
-void countCharactersCategory(char s[])
-{
-	// Initializing variables to count the total number of special
-    // characters, digits, vowels, and consonants in the given string
-    int totalSpecialCharacters = 0, totalDigits = 0, totalVowels = 0, totalConsonants = 0;
-
-	for (int i = 0; i < strlen(s); i++)
-    {
-		char c = s[i];
-
-        // Alphabets family
-		if ( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') )
-        {
-			// Converting character to lower case
-			c = tolower(c);
-
-            // Vowels
-			if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
-            {
-                totalVowels++;
-            }
-            // Consonants
-			else
-            {
-                totalConsonants++;
-            }
-		}
-        // Digits family
-		else if (c >= '0' && c <= '9')
-        {
-            totalDigits++;
-        }
-        // Special characters family
-		else
-        {
-            totalSpecialCharacters++;
-        }
-	}
-    printf("Total no. of vowels in the given string: %d \n",totalVowels);
-    printf("Total no. of consonants in the given string: %d \n",totalConsonants);
-    printf("Total no. of digits in the given string: %d \n",totalDigits);
-    printf("Total no. of special characters in the given string: %d \n",totalSpecialCharacters);
-}
-
-// Driver code
 int main()
 {
-    // Test case: 1
-	char s1[] = "Welcome 2 #MUO";
+  int vowel_count = 0;
+  int consonant_count = 0;
+  char s[100];
+  gets(s);
+  for(int i=0;i<strlen(s);i++)
+    {
+      if(s[i] >=65 && s[i] <=90) s[i]+=32;
+      
+      if(s[i] == 'a'|| s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') vowel_count++;
 
-    printf("Input string: %s \n",s1);
-	countCharactersCategory(s1);
-
-    // Test case: 2
-	char s2[] = "This Is @ InpuT String 2";
-    printf("Input string: %s \n",s2);
-	countCharactersCategory(s2);
-
-	return 0;
-}
+      else consonant_count++;
+    }
+  printf("Vowel count is: %d\n",vowel_count);
+  printf("Consonant count is: %d\n",consonant_count);
+  
+  }
