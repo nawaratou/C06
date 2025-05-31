9d42915104536b75cd6a2162eb0b5418bcf0f16f
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-31 12:01:41 +0100

    trie

---  count_letters.c
+++  count_letters.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
-#include <string.h>
+#include <ctype.h>
 
 int main() {
-    char name[50];
-    int i, vc = 0, cc = 0;
+    char str[100];
+    int i = 0, voyelles = 0, consonnes = 0;
 
-    // Lecture d’une chaîne avec espaces :
-    scanf("%49s", name);  // "%s" ne lit pas les espaces. Si tu veux une phrase entière : "%49[^\n]"
+    // Lire une ligne entière sans message
+    scanf(" %[^\n]", str);
 
-    for (i = 0; i < strlen(name); i++) {
-        if (name[i] == 'a' || name[i] == 'e' || name[i] == 'i' || name[i] == 'o' || name[i] == 'u' ||
-            name[i] == 'A' || name[i] == 'E' || name[i] == 'I' || name[i] == 'O' || name[i] == 'U') {
-            vc++;
-        } else {
-            cc++;
+    // Balayage caractère par caractère
+    while (str[i] != '\0') {
+        char ch = str[i];
+
+        if (isalpha(ch)) {  // Si c'est une lettre
+            ch = tolower(ch);  // Ignorer la casse
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                voyelles++;
+            else
+                consonnes++;
         }
+        i++;
     }
 
-    printf("Voyelles : %d\n", vc);
-    printf("Consonnes : %d\n", cc);
+    printf("Voyelles : %d\n", voyelles);
+    printf("Consonnes : %d\n", consonnes);
 
     return 0;
 }
